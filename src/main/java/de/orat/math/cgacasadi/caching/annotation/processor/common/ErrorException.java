8e851f36e9f2fb87966fc73f18ad2aca75c2d4e1
codedeedope <26957536+codedeedope@users.noreply.github.com>
2024-05-22 16:00:00 +0200

    Minor change

--- src/main/java/de/orat/math/cgacasadi/caching/annotation/processor/common/ErrorException.java
+++ src/main/java/de/orat/math/cgacasadi/caching/annotation/processor/common/ErrorException.java
@@ -1,31 +1,26 @@
 package de.orat.math.cgacasadi.caching.annotation.processor.common;
 
 import javax.lang.model.element.Element;
-import javax.lang.model.element.TypeElement;
 
 public class ErrorException extends Exception {
 
-    public static Exception create(TypeElement correspondingElement) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public final Element element;
+
+    protected ErrorException(Element element, String message, Object... args) {
+        super(String.format(message, args));
+        this.element = element;
     }
 
-	public final Element element;
+    protected ErrorException(Element element, Throwable cause, String message, Object... args) {
+        super(String.format(message, args), cause);
+        this.element = element;
+    }
 
-	protected ErrorException(Element element, String message, Object... args) {
-		super(String.format(message, args));
-		this.element = element;
-	}
+    public static ErrorException create(Element element, String message, Object... args) {
+        return new ErrorException(element, message, args);
+    }
 
-	protected ErrorException(Element element, Throwable cause, String message, Object... args) {
-		super(String.format(message, args), cause);
-		this.element = element;
-	}
-
-	public static ErrorException create(Element element, String message, Object... args) {
-		return new ErrorException(element, message, args);
-	}
-
-	public static ErrorException create(Element element, Throwable cause, String message, Object... args) {
-		return new ErrorException(element, cause, message, args);
-	}
+    public static ErrorException create(Element element, Throwable cause, String message, Object... args) {
+        return new ErrorException(element, cause, message, args);
+    }
 }
