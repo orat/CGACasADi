5e0182bfcc6474be3fdfc0eced2701764673fb96
codedeedope <26957536+codedeedope@users.noreply.github.com>
2024-10-14 21:33:42 +0200

    Cleanup imports

--- CGACasADi_SymbolicMultivectorCachingProcessor/src/main/java/de/orat/math/cgacasadi/caching/annotation/processor/representation/Clazz.java
+++ CGACasADi_SymbolicMultivectorCachingProcessor/src/main/java/de/orat/math/cgacasadi/caching/annotation/processor/representation/Clazz.java
@@ -5,12 +5,9 @@ import de.orat.math.cgacasadi.caching.annotation.processor.common.ErrorException
 import de.orat.math.cgacasadi.caching.annotation.processor.common.FailedToCacheException;
 import static de.orat.math.cgacasadi.caching.annotation.processor.generation.Classes.T_iMultivectorSymbolic;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
