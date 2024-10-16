f8daf78cfa1c1faf51c817e5bec90c50b65d471c
codedeedope <26957536+codedeedope@users.noreply.github.com>
2024-10-16 17:30:40 +0200

    Added numeric delegating processor as copy of caching processor

---
+++ CGACasADi_NumericMultivectorDelegatingProcessor/src/main/java/de/orat/math/cgacasadi/delegating/annotation/processor/generation/Classes.java
@@ -0,0 +1,20 @@
+package de.orat.math.cgacasadi.delegating.annotation.processor.generation;
+
+import com.squareup.javapoet.ClassName;
+import java.util.List;
+
+public final class Classes {
+
+    private Classes() {
+
+    }
+
+    public static final ClassName T_CGASymbolicFunctionCache = ClassName.get("de.orat.math.cgacasadi.caching", "CGASymbolicFunctionCache");
+    public static final ClassName T_ISafePublicFunctionCache = ClassName.get("de.orat.math.cgacasadi.caching", "ISafePublicFunctionCache");
+    public static final ClassName T_SX = ClassName.get("de.dhbw.rahmlab.casadi.impl.casadi", "SX");
+    public static final ClassName T_iMultivectorSymbolic = ClassName.get("de.orat.math.gacalc.spi", "iMultivectorSymbolic");
+
+    public static final ClassName T_String = ClassName.get(String.class);
+    public static final ClassName T_Override = ClassName.get(Override.class);
+    public static final ClassName T_List = ClassName.get(List.class);
+}
