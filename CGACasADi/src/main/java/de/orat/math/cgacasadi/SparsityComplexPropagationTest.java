191acf6593d77718a4a4bd818204013c2d7f69ee
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-10-23 14:13:29 +0200

    Clarified zero test 2

--- CGACasADi/src/main/java/de/orat/math/cgacasadi/SparsityComplexPropagationTest.java
+++ CGACasADi/src/main/java/de/orat/math/cgacasadi/SparsityComplexPropagationTest.java
@@ -5,10 +5,14 @@ import de.dhbw.rahmlab.casadi.impl.casadi.SX;
 import de.orat.math.cgacasadi.impl.CGAExprGraphFactory;
 
 public class SparsityComplexPropagationTest {
+
     public static void main(String[] args) {
         var fac = CGAExprGraphFactory.instance;
         var a = fac.createMultivectorPurelySymbolic("a", 0);
         var b = fac.createMultivectorPurelySymbolic("b", 0);
+
+        var withoutNum = a.sub(a);  // Evaluiert zu struktureller Null.
+        System.out.println(withoutNum);
 
         SX withScalar = a.add(a.gpWithScalar(-2)).add(a).getSX();
         SX simpleSX = SxStatic.simplify(withScalar); // Evaluiert zu numerischer Null.
