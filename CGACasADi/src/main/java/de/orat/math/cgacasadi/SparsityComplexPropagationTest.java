74c37805d3f6405313946d819bbb015bd759ccb5
codedeedope <26957536+codedeedope@users.noreply.github.com>
2025-10-23 13:54:35 +0200

    Clarified zero test

--- CGACasADi/src/main/java/de/orat/math/cgacasadi/SparsityComplexPropagationTest.java
+++ CGACasADi/src/main/java/de/orat/math/cgacasadi/SparsityComplexPropagationTest.java
@@ -1,25 +1,23 @@
 package de.orat.math.cgacasadi;
 
+import de.dhbw.rahmlab.casadi.SxStatic;
+import de.dhbw.rahmlab.casadi.impl.casadi.SX;
 import de.orat.math.cgacasadi.impl.CGAExprGraphFactory;
-import java.util.Objects;
 
 public class SparsityComplexPropagationTest {
     public static void main(String[] args) {
         var fac = CGAExprGraphFactory.instance;
         var a = fac.createMultivectorPurelySymbolic("a", 0);
         var b = fac.createMultivectorPurelySymbolic("b", 0);
-        var sub1 = a.sub(b);
-        var sub2 = a.square().sub(a.square());
-        System.out.println(a);
-        System.out.println(sub1);
-        System.out.println(sub2);
-        System.out.println(Objects.toIdentityString(a.square()));
-        System.out.println(Objects.toIdentityString(a.square()));
-        var c = fac.createMultivectorNumeric(5.5);
-        var subNum = c.square().sub(c.square());
-        var nonZeroExpr = c.square();
-        System.out.println(c);
-        System.out.println(subNum);
-        System.out.println(nonZeroExpr);
+
+        SX withScalar = a.add(a.gpWithScalar(-2)).add(a).getSX();
+        SX simpleSX = SxStatic.simplify(withScalar); // Evaluiert zu numerischer Null.
+        System.out.println(withScalar);
+        System.out.println(simpleSX);
+
+        SX onlyPure = a.add(a.gpWithScalar(-1)).getSX(); // Evaluiert zu numerischer Null.
+        SX onlyPureSym = SxStatic.simplify(onlyPure);
+        System.out.println(onlyPure);
+        System.out.println(onlyPureSym);
     }
 }
